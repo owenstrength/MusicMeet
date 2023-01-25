9168d288d9f34be121ab8d9d835e60c578bed563
Owen Strength <Owenstrength@gmail.com>
2023-01-25 16:39:36 -0600

    Added Image Rendering and Fixed Scrolling Bug

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -9,6 +9,7 @@ const MainScreen = () => {
     <SafeAreaView style={styles.container} >
       <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }} >
         <Text style={styles.name} >User.name</Text>
+        {/*Genre Display Section */}
         <Text style={styles.headingText}>Top Genres</Text>
             <ScrollView horizontal={true} bar showsHorizontalScrollIndicator={false} contentContainerStyle={{width:screenWidth}}>
                 <Text style={styles.genreText}>Genre</Text>
@@ -17,6 +18,7 @@ const MainScreen = () => {
                 <Text style={styles.genreText}>Genre</Text>
                 <Text style={styles.genreText}>Genre</Text>
             </ScrollView>
+            {/*Artist Display Section */}
         <Text style={styles.headingText}>Top Artists Past 4 Weeks</Text>
             <ScrollView horizontal={true} bar showsHorizontalScrollIndicator={false} contentContainerStyle={{width:screenWidth}}>
                 <Image source={"https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea"} style={styles.images}></Image>
@@ -24,6 +26,7 @@ const MainScreen = () => {
                 <Image source={"https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea"} style={styles.images}></Image>
                 <Image source={"https://i.scdn.co/image/ab6761610000e5ebfc9d2abc85b6f4bef77f80ea"} style={styles.images}></Image>
             </ScrollView>
+            {/*Song Display Section */}
         <Text style={styles.headingText}>Top Songs Past 4 Weeks</Text>
             <ScrollView horizontal={true} bar showsHorizontalScrollIndicator={false} contentContainerStyle={{width:screenWidth}}>
                 <Image source={"https://i.scdn.co/image/ab67616d0000b2733db28ea90ddea7e6b333b4aa"} style={styles.images}></Image>
@@ -31,7 +34,9 @@ const MainScreen = () => {
                 <Image source={"https://i.scdn.co/image/ab67616d0000b2733db28ea90ddea7e6b333b4aa"} style={styles.images}></Image>
                 <Image source={"https://i.scdn.co/image/ab67616d0000b2733db28ea90ddea7e6b333b4aa"} style={styles.images}></Image>
             </ScrollView>
+            {/* User Chosen Playlist*/}
         <Text style={styles.headingText}>User.name's playlist</Text>
+            {/* User Chosen Socials*/}
         <Text style={styles.headingText}>Socials</Text>
         
         </ScrollView>
